f296d8ed3bf81728f06d0c5c9280ada8ae134ad5
BLGALEX <lepehaleha@bk.ru>
2022-01-29 00:20:21 +0900

    new constant added

--- translator/constants.py
+++ translator/constants.py
@@ -5,6 +5,11 @@ ARITHMETIC_OPERATORS = ['+', '-', '*', '/', '%']
 LOGICAL_OPERATORS = ['||', '&&']
 COMPARISON_OPERATORS = ['<', '>', '==', '!=', '<=', '>=']
 ASSIGNMENT_OPERATORS = ['=', '+=', '-=', '*=', '/=', '%=']
+OTHER_SYMBOLS = ['_', '!', '@', '#', '$', '%', '^', '&',
+                 '*', '(', ')', '-', '+', ']', '[', '{',
+                 '}', '№', ';', ':', '?', '=', '<', '>',
+                 '.', ',']
+VARIABLE_TYPES = ['int', 'float', 'char', 'bool', 'double', 'String']
 LETTERS = list(string.ascii_letters)
 
 OPERATORS = UNARY_OPERATORS + ARITHMETIC_OPERATORS + LOGICAL_OPERATORS + COMPARISON_OPERATORS + ASSIGNMENT_OPERATORS
