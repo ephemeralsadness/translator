d6263a10b4662725fd2526a65bfe95c76a6e53aa
Alexander Sarantcev <sarancev.dev@gmail.com>
2022-01-29 05:17:46 +1000

    new constant added: KEYWORDS

--- translator/constants.py
+++ translator/constants.py
@@ -13,3 +13,5 @@ VARIABLE_TYPES = ['int', 'float', 'char', 'boolean', 'double', 'String']
 LETTERS = list(string.ascii_letters)
 
 OPERATORS = UNARY_OPERATORS + ARITHMETIC_OPERATORS + LOGICAL_OPERATORS + COMPARISON_OPERATORS + ASSIGNMENT_OPERATORS
+
+KEYWORDS = VARIABLE_TYPES + 'class do else for if public return static while'.split(' ')
